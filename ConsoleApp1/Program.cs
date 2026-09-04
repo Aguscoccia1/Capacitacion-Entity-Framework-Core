62b9f7f85e553522469479252a8e2fa6b0d3deb8
Agustin Coccia <aguscoccia1905@gmail.com>
2026-09-03 21:04:30 -0300

    Se organiza las operaciones CRUD en funciones

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -23,8 +23,10 @@ class Program
 
             // esto trae a los proyectos e incluye solo las tareas que no estén completadas
             var proyectosConTareasPendientes = await context.Proyectos
-                .Include(p => p.Tareas.Where(t => !t.Completada)) 
+                .Include(p => p.Tareas.Where(t => !t.Completada))
                 .ToListAsync();
+
+            //se llama a las funciones
 
             //Operacion 1: lectura y filtrado 
 
@@ -38,7 +40,7 @@ class Program
 
                 if (proyecto.Tareas.Any())
                 {
-                    foreach (var tareaPendiente in proyecto.Tareas) 
+                    foreach (var tareaPendiente in proyecto.Tareas)
                     {
                         Console.WriteLine($"Tarea Pendiente: {tareaPendiente.Titulo} - ID: {tareaPendiente.Id}");
                     }
@@ -57,73 +59,91 @@ class Program
             Console.WriteLine("¿qué tarea quieres completar?(ingrese el id)");
             int tareaIdACompletar = int.Parse(Console.ReadLine());
 
-            var tareaSeleccionada = await context.Tareas.FindAsync(tareaIdACompletar);
-
-            if (tareaSeleccionada != null)
-            {
-                Console.WriteLine($"Estado original de '{tareaSeleccionada.Titulo}': Completada = {tareaSeleccionada.Completada}");
-
-                tareaSeleccionada.Completada = true;
-
-                // Guardamos los cambios asincrónicamente en SQL Server
-                await context.SaveChangesAsync();
-
-                Console.WriteLine($"Estado actualizado con éxito");
-            }
-            else
-            {
-                Console.WriteLine($"No se encontró ninguna tarea con el ID {tareaIdACompletar}, por favor ingrese un ID válido");
-            }
-
+            await CompletarTarea(context, tareaIdACompletar);
 
             // Operacion 3: Eliminación en Cascada 
 
             Console.WriteLine();
             Console.WriteLine("Eliminacion en cascada");
 
-            foreach (var proyecto in proyectosConTareasPendientes)
+            Console.WriteLine("¿Que proyecto quieres eliminar? (ingresa el ID)");
+            foreach(var proyecto in proyectosConTareasPendientes)
             {
-                Console.WriteLine($"Proyecto: {proyecto.Nombre} - ID: {proyecto.Id}");
-                foreach (var tarea in proyecto.Tareas)
+                Console.WriteLine();
+                Console.WriteLine($"Proyecto: {proyecto.Nombre} - {proyecto.Id}");
+
+                if (proyecto.Tareas.Any())
                 {
-                    Console.WriteLine($"Tarea Pendiente: {tarea.Titulo} - ID: {tarea.Id}");
+                    foreach (var tareaPendiente in proyecto.Tareas)
+                    {
+                        Console.WriteLine($"Tarea Pendiente: {tareaPendiente.Titulo} - ID: {tareaPendiente.Id}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Este proyecto no tiene tareas pendientes");
                 }
             }
 
-            Console.WriteLine("¿Que proyecto quieres eliminar? (ingresa el ID)");
             int idProyectoAEliminar = int.Parse(Console.ReadLine());
 
-            // Buscamos el primer proyecto disponible para borrarlo
-            var proyectoParaBorrar = await context.Proyectos.FirstOrDefaultAsync(p => p.Id == idProyectoAEliminar);
-
-            if (proyectoParaBorrar != null)
-            {
-                int idEliminado = proyectoParaBorrar.Id;
-                string nombreEliminado = proyectoParaBorrar.Nombre;
-
-                Console.WriteLine($"Proyecto a eliminar: '{nombreEliminado}' - ID: {idEliminado}");
-
-                // se elimina el proyecto del contexto
-                context.Proyectos.Remove(proyectoParaBorrar);
-
-                // Guardamos cambios (esto dispara el borrado en cascada en la base de datos)
-                await context.SaveChangesAsync();
-
-                Console.WriteLine($"Proyecto '{nombreEliminado}' eliminado");
-
-                // Se verifica si quedaron tareas asociadas al id del proyecto borrado en la base de datos
-                int tareasSueltas = await context.Tareas.CountAsync(t => t.ProyectoId == idEliminado);
-
-                Console.WriteLine($"Tareas en la base de datos vinculadas al ID {idEliminado} = {tareasSueltas}");
-            }
-            else
-            {
-                Console.WriteLine("No hay proyectos en la base de datos para eliminar.");
-            }
+            await EliminarProyecto(context, idProyectoAEliminar);
 
             //El readkey es para que al apretar una tecla pase a lo siguiente, en este caso termina,de lo contrario la terminal se cierra sola.
             Console.WriteLine("Presione una tecla para salir...");
             Console.ReadKey();
+        }
+    }
+
+    static async Task CompletarTarea(AppDbContext context, int tareaId)
+    {
+        var tareaSeleccionada = await context.Tareas.FindAsync(tareaId);
+
+        if (tareaSeleccionada != null)
+        {
+            Console.WriteLine($"Estado original de '{tareaSeleccionada.Titulo}': Completada = {tareaSeleccionada.Completada}");
+
+            tareaSeleccionada.Completada = true;
+
+            // Guardamos los cambios asincrónicamente en SQL Server
+            await context.SaveChangesAsync();
+
+            Console.WriteLine($"Tarea '{tareaSeleccionada.Titulo}' completada correctamente.");
+        }
+        else
+        {
+            Console.WriteLine($"No se encontró ninguna tarea con el ID {tareaId}, por favor ingrese un ID válido");
+        }
+    }
+
+    static async Task EliminarProyecto(AppDbContext context, int proyectoId)
+    {
+        var proyectoParaBorrar = await context.Proyectos
+            .FirstOrDefaultAsync(p => p.Id == proyectoId);
+
+        if (proyectoParaBorrar != null)
+        {
+            int idEliminado = proyectoParaBorrar.Id;
+            string nombreEliminado = proyectoParaBorrar.Nombre;
+
+            Console.WriteLine($"Proyecto a eliminar: '{nombreEliminado}' - ID: {idEliminado}");
+
+            // se elimina el proyecto del contexto
+            context.Proyectos.Remove(proyectoParaBorrar);
+
+            // Guardamos cambios (esto dispara el borrado en cascada en la base de datos)
+            await context.SaveChangesAsync();
+
+            Console.WriteLine($"Proyecto '{nombreEliminado}' eliminado");
+
+            // Se verifica si quedaron tareas asociadas al id del proyecto borrado en la base de datos
+            int tareasSueltas = await context.Tareas.CountAsync(t => t.ProyectoId == idEliminado);
+
+            Console.WriteLine($"Tareas en la base de datos vinculadas al ID {idEliminado} = {tareasSueltas}");
+        }
+        else
+        {
+            Console.WriteLine("No hay proyectos en la base de datos para eliminar.");
         }
     }
 }
